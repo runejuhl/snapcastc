4485ecab938185464b441c54c4a8b50ee6ef8d8e
Christof Schulze <christof.schulze@gmx.net>
2019-04-17 20:24:18 +0200

    fixup! alsaplayer: Allow Client to run without mixer

--- src/alsaplayer.c
+++ src/alsaplayer.c
@@ -324,7 +324,10 @@ void mixer_init(alsaplayer_ctx *ctx) {
 	if (ctx->mixer_elem)
 		snd_mixer_selem_get_playback_volume_range(ctx->mixer_elem, &ctx->mixer_min, &ctx->mixer_max);
 	else {
-		log_error("could not initialize mixer %s. Continuing with bogus volume values. You will not be able to control the volume\n.", ctx->mixer);
+		ctx->is_softvol = true;
+		ctx->softvol_level = 100;
+		log_error("could not initialize mixer %s. Continuing with bogus volume values. You will not be able to control the volume\n.",
+			  ctx->mixer);
 	}
 }
 
@@ -335,22 +338,28 @@ void mixer_uninit(alsaplayer_ctx *ctx) {
 
 uint8_t obtain_volume(alsaplayer_ctx *ctx) {
 	long volume = 0;
-	uint8_t ret = 100;
-	mixer_init(ctx);
-	if (ctx->mixer_elem) {
-		snd_mixer_selem_get_playback_volume(ctx->mixer_elem, SND_MIXER_SCHN_MONO, &volume);
-		ret = (uint8_t)(round((double)volume * 100 / ctx->mixer_max));
-		log_debug("Obtained volume (raw): %d\n", ret);
+	uint8_t ret = ctx->softvol_level;
+	if (!ctx->is_softvol) {
+		mixer_init(ctx);
+		if (ctx->mixer_elem) {
+			snd_mixer_selem_get_playback_volume(ctx->mixer_elem, SND_MIXER_SCHN_MONO, &volume);
+			ret = (uint8_t)(round((double)volume * 100 / ctx->mixer_max));
+			log_debug("Obtained volume (raw): %d\n", ret);
+		}
+		mixer_uninit(ctx);
 	}
-	mixer_uninit(ctx);
 	return ret;
 }
 
 void adjustVolume(alsaplayer_ctx *ctx, uint8_t volume) {
-	mixer_init(ctx);
-	if (ctx->mixer_elem)
-		snd_mixer_selem_set_playback_volume_all(ctx->mixer_elem, volume * ctx->mixer_max / 100);
-	mixer_uninit(ctx);
+	if (ctx->is_softvol) {
+		ctx->softvol_level = volume;
+	} else {
+		mixer_init(ctx);
+		if (ctx->mixer_elem)
+			snd_mixer_selem_set_playback_volume_all(ctx->mixer_elem, volume * ctx->mixer_max / 100);
+		mixer_uninit(ctx);
+	}
 }
 
 void alsaplayer_init(alsaplayer_ctx *ctx) {
